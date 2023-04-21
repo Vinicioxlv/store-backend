3cbaac5705092c67bc316c202723e3e74d76eb6e
Vinicio <vinicioxavierlv@gmail.com>
2023-04-21 18:53:11 -0500

    se agrega registro de usuarios y autenticacion

--- apis/index.js
+++ apis/index.js
@@ -1,7 +1,18 @@
 const express = require('express')
 const router = express.Router()
 
+const userRouter = require('./users')
+const authRouter = require('./auth')
+const authMiddleware = require('../middleware/authorization') 
+const registerRouter = require('./register')
+
 const productsRouter = require('./products')
+
+router.use('/auth', authRouter)
+router.use('/register', registerRouter)
+
+router.use(authMiddleware)
+router.use('/user', userRouter)
 
 router.use('/products', productsRouter)
 
