6704f672bd4371028c524b3112346562cc25dd55
Vinicio <vinicioxavierlv@gmail.com>
2023-04-28 11:32:38 -0500

    se agrega archivo vercel

--- apis/index.js
+++ apis/index.js
@@ -2,18 +2,20 @@ const express = require('express')
 const router = express.Router()
 
 const userRouter = require('./users')
+const productsRouter = require('./products')
 const authRouter = require('./auth')
 const authMiddleware = require('../middleware/authorization') 
 const registerRouter = require('./register')
 
-const productsRouter = require('./products')
+
 
 router.use('/auth', authRouter)
 router.use('/register', registerRouter)
+router.use('/products', productsRouter)
 
 router.use(authMiddleware)
 router.use('/user', userRouter)
 
-router.use('/products', productsRouter)
+
 
 module.exports = router
